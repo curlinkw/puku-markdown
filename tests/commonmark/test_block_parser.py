2cba05ff2e5ef7682f52fbe089c4fce2c9160b59
curlinkw <p.kurochkin200@gmail.com>
2026-06-04 18:20:49 +0300

    WIP

--- tests/commonmark/test_block_parser.py
+++ tests/commonmark/test_block_parser.py
@@ -2,6 +2,9 @@ import pytest
 from markdown_it import MarkdownIt
 
 from tests.commonmark.spec import SpecExample, get_all_examples
+from tests.markdown_it_py import markdown_it_py_block_parse, BlockToken
+
+from pmark.parser.parse import parse
 
 
 @pytest.mark.parametrize("spec_example", get_all_examples(), ids=lambda x: x.example)
@@ -9,6 +12,8 @@ def test_block_parser(spec_example: SpecExample) -> None:
     if spec_example.example in (596, 218, 239, 240):
         return None
 
-    markdown_it_output = MarkdownIt("commonmark").render(spec_example.markdown)
+    assert MarkdownIt("commonmark").render(spec_example.markdown) == spec_example.html
 
-    assert markdown_it_output == spec_example.html
+    assert markdown_it_py_block_parse(
+        spec_example.markdown
+    ) == BlockToken.from_document(parse(spec_example.markdown))
