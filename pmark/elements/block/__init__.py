a8f3a3e71f108352a974577f5fa5ddfa6441f2ed
curlinkw <p.kurochkin200@gmail.com>
2026-06-04 17:37:09 +0300

    add: __init__.py

--- pmark/elements/block/__init__.py
+++ pmark/elements/block/__init__.py
@@ -1,6 +1,34 @@
 from pmark.elements.block.base import BlockElement
+from pmark.elements.block.commonmark import (
+    AtxHeading,
+    Blockquote,
+    FencedCodeBlock,
+    HtmlBlock,
+    HtmlBlockKind,
+    IdentedCodeBlock,
+    LinkReferenceDefinition,
+    List,
+    ListItem,
+    ListKind,
+    ThematicBreak,
+    SetextHeading,
+    Paragraph,
+)
 
 
 __all__ = [
-    "BlockElement"
+    "BlockElement",
+    "AtxHeading",
+    "Blockquote",
+    "FencedCodeBlock",
+    "HtmlBlock",
+    "HtmlBlockKind",
+    "IdentedCodeBlock",
+    "LinkReferenceDefinition",
+    "List",
+    "ListItem",
+    "ListKind",
+    "ThematicBreak",
+    "SetextHeading",
+    "Paragraph",
 ]
---
+++ pmark/elements/block/commonmark/__init__.py
@@ -0,0 +1,29 @@
+from pmark.elements.block.commonmark.atx_heading import AtxHeading
+from pmark.elements.block.commonmark.blockquote import Blockquote
+from pmark.elements.block.commonmark.fenced_code_block import FencedCodeBlock
+from pmark.elements.block.commonmark.html_block import HtmlBlock, HtmlBlockKind
+from pmark.elements.block.commonmark.indented_code_block import IdentedCodeBlock
+from pmark.elements.block.commonmark.link_reference_definition import (
+    LinkReferenceDefinition,
+)
+from pmark.elements.block.commonmark.list import List, ListItem, ListKind
+from pmark.elements.block.commonmark.thematic_break import ThematicBreak
+from pmark.elements.block.commonmark.setext_heading import SetextHeading
+from pmark.elements.block.commonmark.paragraph import Paragraph
+
+
+__all__ = [
+    "AtxHeading",
+    "Blockquote",
+    "FencedCodeBlock",
+    "HtmlBlock",
+    "HtmlBlockKind",
+    "IdentedCodeBlock",
+    "LinkReferenceDefinition",
+    "List",
+    "ListItem",
+    "ListKind",
+    "ThematicBreak",
+    "SetextHeading",
+    "Paragraph",
+]
